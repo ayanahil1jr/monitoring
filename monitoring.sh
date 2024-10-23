aeb715772cd530a13e11b09ea8e9bfdecb31d573
ayan <ayatif@smail.pembinatrails.ca>
2024-10-23 13:56:36 -0500

    Added a system to choose other options after choosing one.

--- monitoring.sh
+++ monitoring.sh
@@ -7,7 +7,6 @@ echo "Available space"
 echo "Available memory"
 
 read input
-clear
 if [ "$input" != "CPU and memory usage" ] && [ "$input" != "Available space" ] && [ "$input" != "Available memory" ]; then
 echo "invalid input"
 sleep 1
@@ -22,7 +21,31 @@ htop
 elif [ "$input" == "Available space" ]; then
 df -h
 elif [ "$input" == "Available memory" ]; then
-
-
 free -h
 fi
+sleep 1
+echo ""
+echo ""
+echo ""
+while true; do
+echo "click Enter to escape or choose another option"
+sleep 1
+echo "CPU and memory usage"
+echo "Available space"
+echo "Available memory"
+
+read redo
+clear
+if [ "$redo" == "" ]; then
+	exit 0
+elif [ "$redo" == "CPU and memory usage" ]; then
+                htop
+elif [ "$redo" == "Available space" ]; then
+                df -h
+elif [ "$redo" == "Available memory" ]; then
+                free -h
+elif [ "$redo" != "CPU and memory usage" ] && [ "$redo" != "Available space" ] && [ "$redo" != "Available memory" ]; then
+	echo "invalid input" 
+fi
+
+done
